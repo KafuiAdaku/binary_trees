257594524ec7b01e97f3ed1a3693510440319262
Kafui Adaku <dennisadaku@ymail.com>
2023-08-31 13:59:24 -0700

    added solution to task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -26,3 +26,26 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	else
 		return (parent->left);
 }
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: a pointer to the uncle node
+ * If node is NULL, return NULL
+ * If node has no uncle, return NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent = NULL;
+	binary_tree_t *uncle = NULL;
+
+	if (!node || !(node->parent))
+		return (NULL);
+
+	parent = node->parent;
+
+	uncle = binary_tree_sibling(parent);
+
+	return (uncle);
+}
